f91b738ebe9950db2f2fda16242902c869bc3ce9
TenJava Plugin Contest 2014 <tenjava@lol768.com>
2014-06-08 18:31:35 +0100

    Initial commit

--- src/main/java/com/tenjava/entries/Ollywood/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/Ollywood/t2/TenJava.java
@@ -2,4 +2,16 @@ package com.tenjava.entries.Ollywood.t2;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	
+	public void onDisable() {
+		
+	}
+	
+	
+}
