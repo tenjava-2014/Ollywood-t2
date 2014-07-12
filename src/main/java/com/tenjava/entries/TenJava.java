cf4e0744dc6e04a79beffd0ee60f7e15e04e5869
Ollywood <ofilby@gmail.com>
2014-07-12 13:58:54 +0100

    TenJava

--- src/main/java/com/tenjava/entries/TenJava.java
+++ src/main/java/com/tenjava/entries/TenJava.java
@@ -1,5 +1,12 @@
 package com.tenjava.entries;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TenJava extends JavaPlugin {
@@ -8,7 +15,53 @@ public class TenJava extends JavaPlugin {
 		
 	}
 	
-	
-	
-	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandString, String[] args) {
+		
+		if (!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED+"Console cannot create new game!");
+		}
+		
+		final Player p = (Player) sender;
+		
+		if(commandString.equalsIgnoreCase("new")) {
+			
+			p.getInventory().clear();
+			
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+				 
+				  public void run() {
+				      if (p.getInventory().contains(Material.DIAMOND)) {
+				    	  
+				    	  ItemStack[] inv = p.getInventory().getContents();
+                          
+				    	    int cuantity= 0;
+				    	    for(int i = 0; i < inv.length; i++) {
+				    	        if(inv[i] != null){
+				    	            if( inv[i].getType() == Material.DIAMOND) {
+				    	                int cant = inv[i].getAmount();
+				    	                cuantity= cuantity + cant;
+				    	            }
+				    	        }
+				    	    }
+				    	  
+				    	  p.sendMessage(ChatColor.GOLD+"Well done! You found "+cuantity+" diamond!");
+				      } else {
+				    	  p.sendMessage(ChatColor.RED+"Bad luck! Try better next time!");
+				      }
+				  }
+				}, 200L);
+			
+		}
+		
+		
+		
+		
+		return false;
+	}
+
 }
+
+	
+	
+
